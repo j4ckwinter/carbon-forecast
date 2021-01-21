01470d8ed7aa5db3d910695080a978bd56b8df5c
j4ckwinter <jackwinter.10@gmail.com>
2021-01-21 17:21:13 +0000

    #1 Setup linting rules and prettier

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,23 +1,32 @@
-import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
+import { AppPage } from './app.po';
 
-describe('workspace-project App', () => {
+describe('workspace-project App', (): void => {
   let page: AppPage;
 
-  beforeEach(() => {
+  beforeEach((): void => {
     page = new AppPage();
   });
 
-  it('should display welcome message', async () => {
+  it('should display welcome message', async (): Promise<void> => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('carbon-forecast app is running!');
+    expect(await page.getTitleText()).toEqual(
+      'carbon-forecast app is running!'
+    );
   });
 
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
-  });
+  afterEach(
+    async (): Promise<void> => {
+      // Assert that there are no errors emitted from the browser
+      const logs: logging.Entry[] = await browser
+        .manage()
+        .logs()
+        .get(logging.Type.BROWSER);
+      expect(logs).not.toContain(
+        jasmine.objectContaining({
+          level: logging.Level.SEVERE,
+        } as logging.Entry)
+      );
+    }
+  );
 });
